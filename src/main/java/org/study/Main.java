8ebda70428526c1d853bec89291dc7a24f84b930
Maycon Douglas <maycondss@live.com>
2023-10-18 14:36:30 +0100

    chore: calling Methods and adding info

--- src/main/java/org/study/Main.java
+++ src/main/java/org/study/Main.java
@@ -1,13 +1,17 @@
 package org.study;
 
 import org.study.basics.ControlFlow;
+import org.study.basics.Methods;
 import org.study.basics.Operator;
 import org.study.basics.Types;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("to better understand the code, please read the comments in the source files");
+        System.out.println("To run the code and understand it, please run on debug mode");
         new Operator();
         new Types();
+        new Methods();
         new ControlFlow();
 
     }
