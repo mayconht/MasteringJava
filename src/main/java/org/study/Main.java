00c6469a7a0e8d0f2d29d272ffde39dd9ca41ea0
Maycon Douglas <maycondss@live.com>
2023-10-20 12:03:26 +0100

    chore: organizing main

--- src/main/java/org/study/Main.java
+++ src/main/java/org/study/Main.java
@@ -12,21 +12,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("to better understand the code, please read the comments in the source files");
         System.out.println("To run the code and understand it, please run on debug mode");
-        new Operator();
-        new Types();
-        new Methods();
-        new ControlFlow();
-        new Classes();
-        StaticEx.method1();
-        StaticEx.method("test");
-        System.out.println("StaticEx.a = " + StaticEx.a);
-        StaticEx ex1 = new StaticEx(5);
-        StaticEx ex2 = new StaticEx(15);
-        System.out.println("ex1.a = " + ex1.a);
-        System.out.println("ex2.a = " + ex2.a);
-        Pojo po = new Pojo(1, 2);
-        po.setX(5);
-        System.out.println("po.getX() = " + po.getX());
-
+        {
+            System.out.println("basics");
+            new Operator();
+            new Types();
+            new Methods();
+            new ControlFlow();
+        }
+        {
+            System.out.println("oop");
+            new Classes();
+            StaticEx.method1();
+            StaticEx.method("test");
+            System.out.println("StaticEx.a = " + StaticEx.a);
+            StaticEx ex1 = new StaticEx(5);
+            StaticEx ex2 = new StaticEx(15);
+            System.out.println("ex1.a = " + ex1.a);
+            System.out.println("ex2.a = " + ex2.a);
+            {
+                System.out.println("Pojo");
+                Pojo po = new Pojo(1, 2);
+                po.setX(5);
+                System.out.println("po.getX() = " + po.getX());
+                System.out.println("po.getY() = " + po.getY());
+            }
+        }
     }
 }
