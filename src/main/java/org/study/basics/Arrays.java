bc1d783695e63c26cdceba5e874a951043090014
Maycon Douglas <maycondss@live.com>
2023-11-03 23:04:15 +0000

    Learning from last interview, arrays

--- src/main/java/org/study/basics/Arrays.java
+++ src/main/java/org/study/basics/Arrays.java
@@ -1,5 +1,8 @@
 package org.study.basics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arrays {
     public Arrays() {
         // Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
@@ -39,6 +42,28 @@ public class Arrays {
                 System.out.println(j);
             }
         }
+
+    }
+
+    public void insideArrays() {
+        var simpleArray = new int[10];
+        // when you create an array, you have to specify the size of the array, that way the compiler knows how much memory to allocate for the array
+        // you can also initialize the array with values like this:
+        var initializedArray = new int[]{1, 2, 3, 4, 5};
+        // you can also create a multidimensional array like this:
+        var multiDimensionalArray = new int[10][10];
+        List<Object> list = new ArrayList<>();
+        list.add(1);
+        list.add("hello");
+        list.add(true);
+        list.add(1.0);
+        /*
+        The difference in performance between an array and a list is that an array is a fixed size, so it is faster to access an element in an array than it is to access an element in a list.
+        A list is a dynamic data structure, so it is slower to access an element in a list than it is to access an element in an array.
+        A dinamic List is a list that can grow and shrink in size, but on background it has to create a new array and copy the elements from the old array to the new array.
+        in terms of performance, it is faster to access an element in an array than it is to access an element in a list.
+        that is why you should specify the size of the array when you create it, so that the compiler knows how much memory to allocate for the array and the performance is better.
+         */
 
     }
 
